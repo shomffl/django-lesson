fe7a4230d098a21781b22e26569d1770a0e82311
shomffl <showatanabe0226@gmail.com>
2021-12-28 18:15:33 +0900

    myappの追加

---
+++ django_lesson1/first/myapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+def index(request):
+    context = {
+        "name": "Sho"
+    }
+    return render(request, "myapp/index.html", context)
+
+
+def about(request):
+    return render(request, "myapp/about.html")
+
+
+def info(request):
+    return render(request, "myapp/info.html")

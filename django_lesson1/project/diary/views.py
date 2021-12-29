1a1d98f8b43d06b17c1f664ab0aff2744b3f0fa7
shomffl <showatanabe0226@gmail.com>
2021-12-29 23:03:57 +0900

    日記アプリのfirst commit

---
+++ django_lesson1/project/diary/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from .forms import DayCreateForm
+from .models import Day
+
+def index(request):
+    context ={
+        "day_list": Day.objects.all(),
+    }
+    return render(request, "diary/day_list.html", context)
+
+def add(request):
+
+    form = DayCreateForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        return redirect("diary:index")
+
+    context = {
+        "form": form
+    }
+    return render(request, "diary/day_form.html", context)

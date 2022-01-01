8e2ca8b55365e58fc89c70ec5c30abb4099cdc77
shomffl <showatanabe0226@gmail.com>
2022-01-01 20:04:22 +0900

    削除・詳細ページの追加

--- django_lesson1/project/diary/urls.py
+++ django_lesson1/project/diary/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("add/", views.add, name="add"),
     path("update/<int:pk>/", views.update, name="update"),
-
+    path("delete/<int:pk>/", views.delete, name="delete"),
+    path("detail/<int:pk>/", views.detail, name="detail"),
 ]
--- django_lesson1/project/diary/views.py
+++ django_lesson1/project/diary/views.py
@@ -36,3 +36,27 @@ def update(request, pk):
     }
 
     return render(request, "diary/day_form.html", context)
+
+
+def delete(request, pk):
+    day = get_object_or_404(Day, pk=pk)
+
+    if request.method == "POST":
+        day.delete()
+        return redirect("diary:index")
+
+    context = {
+        "day": day,
+    }
+
+    return render(request, "diary/day_confirm_delete.html", context)
+
+
+def detail(request, pk):
+    day = get_object_or_404(Day, pk=pk)
+
+    context = {
+        "day": day,
+    }
+
+    return render(request, "diary/day_detail.html", context)

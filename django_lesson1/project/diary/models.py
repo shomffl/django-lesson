81a830a15629f02ee701f0ffcd1ac128747ccb85
shomffl <showatanabe0226@gmail.com>
2022-01-01 19:43:17 +0900

    更新機能の追加

--- django_lesson1/project/diary/models.py
+++ django_lesson1/project/diary/models.py
@@ -6,5 +6,3 @@ class Day(models.Model):
     title = models.CharField("タイトル", max_length=200)
     text = models.TextField("本文")
     date = models.DateTimeField("日付", default=timezone.now)
-
-    
--- django_lesson1/project/diary/urls.py
+++ django_lesson1/project/diary/urls.py
@@ -6,5 +6,6 @@ app_name = "diary"
 urlpatterns = [
     path("", views.index, name="index"),
     path("add/", views.add, name="add"),
+    path("update/<int:pk>/", views.update, name="update"),
 
 ]

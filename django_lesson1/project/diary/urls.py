d9ac3f60c18c6c918c9649c783a78602a129b1b2
shomffl <showatanabe0226@gmail.com>
2022-01-01 22:15:22 +0900

    汎用ビューに変更

--- django_lesson1/project/diary/urls.py
+++ django_lesson1/project/diary/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = "diary"
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("add/", views.add, name="add"),
-    path("update/<int:pk>/", views.update, name="update"),
-    path("delete/<int:pk>/", views.delete, name="delete"),
-    path("detail/<int:pk>/", views.detail, name="detail"),
+    path("", views.indexView.as_view(), name="index"),
+    path("add/", views.AddView.as_view(), name="add"),
+    path("update/<int:pk>/", views.UpdateView.as_view(), name="update"),
+    path("delete/<int:pk>/", views.DeleteView.as_view(), name="delete"),
+    path("detail/<int:pk>/", views.DetailView.as_view(), name="detail"),
 ]
--- django_lesson1/project/diary/views.py
+++ django_lesson1/project/diary/views.py
@@ -1,62 +1,29 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views import generic
 from .forms import DayCreateForm
 from .models import Day
 
-def index(request):
-    context ={
-        "day_list": Day.objects.all(),
-    }
-    return render(request, "diary/day_list.html", context)
-
-def add(request):
-
-    form = DayCreateForm(request.POST or None)
-
-    if request.method == "POST" and form.is_valid():
-        form.save()
-        return redirect("diary:index")
-
-    context = {
-        "form": form
-    }
-    return render(request, "diary/day_form.html", context)
+class indexView(generic.ListView):
+    model = Day
 
 
-def update(request, pk):
-    day = get_object_or_404(Day, pk=pk)
-
-    form = DayCreateForm(request.POST or None, instance=day)
-
-    if request.method == "POST" and form.is_valid():
-        form.save()
-        return redirect("diary:index")
-
-    context = {
-        "form": form
-    }
-
-    return render(request, "diary/day_form.html", context)
+class AddView(generic.CreateView):
+    model = Day
+    form_class = DayCreateForm
+    success_url = reverse_lazy("diary:index")
 
 
-def delete(request, pk):
-    day = get_object_or_404(Day, pk=pk)
-
-    if request.method == "POST":
-        day.delete()
-        return redirect("diary:index")
-
-    context = {
-        "day": day,
-    }
-
-    return render(request, "diary/day_confirm_delete.html", context)
+class UpdateView(generic.UpdateView):
+    model = Day
+    form_class = DayCreateForm
+    success_url = reverse_lazy("diary:index")
 
 
-def detail(request, pk):
-    day = get_object_or_404(Day, pk=pk)
+class DeleteView(generic.DeleteView):
+    model = Day
+    success_url = reverse_lazy("diary:index")
 
-    context = {
-        "day": day,
-    }
 
-    return render(request, "diary/day_detail.html", context)
+class DetailView(generic.DetailView):
+    model = Day

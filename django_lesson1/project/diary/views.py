8e0fc60c92b066137581bf53869da8f5b9ddc220
shomffl <showatanabe0226@gmail.com>
2022-01-02 15:05:39 +0900

    管理画面の実装

--- django_lesson1/project/diary/views.py
+++ django_lesson1/project/diary/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic
